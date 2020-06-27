1b00b22b269339c8b0e42975a38c616ec72a17f9
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-06-27 17:07:53 +0900

    기본입출력 연습

---
+++ 구구단.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main ()
+{
+	int dan, n;
+	printf("몇 단을 보고싶으신가요? : ");
+	scanf("%d", &dan);
+	printf("\n%d단입니다.\n", dan);
+	
+	for(n = 1;n <= 9;n++)
+	{
+		printf("%d * %d = %d\n", dan, n, dan * n);
+	}
+	return 0;
+}
---
+++ 정해진 수 만큼의 여러값 더하기.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n, x, sum;
+	printf("더할 수의 갯수를 입력하세요\n");
+	scanf("%d", &n);
+	
+	for(sum = 0;n > 0;n--)
+	{
+		printf("더할 수를 입력하세요. (남은 갯수 : %d) : ", n);
+		scanf("%d", &x);
+		sum += x;
+	}
+	
+	printf("총 합은 %d입니다.\n", sum);
+	return 0;
+}
