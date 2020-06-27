d2a04ecb23baa19c232c6cd4151dc14ae946b779
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-06-28 00:02:07 +0900

    사용자정의함수 연습

---
+++ 시간 더하기.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int m, h, madd;
+
+void counter()
+{
+	m += madd;
+	h += m / 60;
+	m %= 60;
+	h %= 24;
+}
+
+int main()
+{
+	printf("hour을 입력하세요 : ");
+	scanf("%d", &h);
+	printf("\nminute를 입력하세요 :");
+	scanf("%d", &m);
+	printf("\n몇 분을 더하시겠습니까? : ");
+	scanf("%d", &madd);
+	counter();
+	printf("결과는 %d시 %d분입니다.\n", h, m);
+	return 0;
+}
