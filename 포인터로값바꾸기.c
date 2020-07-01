3aeb37b73d57991e5084d1e5e26d8b3793dedcaa
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-07-02 01:36:11 +0900

    포인터 연습

---
+++ 포인터로값바꾸기.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int i = 10;
+	printf("i = %d\n", i);
+	int *ptr;
+	ptr = &i;
+	*ptr = 20;
+	printf("i = %d\n", i);
+	return 0;
+}
---
+++ 포인터로값스왑.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+void swap(int *x, int *y)
+{
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+int main()
+{
+	int x, y;
+	scanf("%d %d", &x, &y);
+	printf("Swap!\n");
+	swap(&x, &y);
+	printf("%d %d",x, y);
+	return 0;
+}
