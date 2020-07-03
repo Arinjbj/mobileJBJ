e25427310e84594fbf4d3f52817758c37364ce15
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-07-03 17:25:33 +0900

    구조체 연습

---
+++ 구조체두점사이거리.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+struct point
+{
+	double x, y;
+};
+
+int main()
+{
+	struct point a, b;
+	printf("1번 좌표(x y)를 입력하세요(ex : 12.4 15.7)\n");
+	scanf("%lf %lf", &a.x, &a.y);
+	printf("2번 좌표(x y)를 입력하세요\n");
+	scanf("%lf %lf", &b.x, &b.y);
+	printf("두 점 사이의 거리는 %g입니다.\n", sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2)));
+	return 0;
+}
---
+++ 구조체학생명단.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+struct student
+{
+	int number;
+	char name[16];
+	float grade;
+};
+
+int main()
+{
+	struct student a;
+	printf("학번을 입력하세요 : ");
+	scanf("%d", &a.number);
+	printf("이름을 입력하세요 : ");
+	scanf("%s", a.name);
+	printf("학점을 입력하세요 : ");
+	scanf("%f", &a.grade);
+	printf("\n학번 : %d\n이름 : %s\n학점 : %.2f\n", a.number, a.name, a.grade);
+	return 0;
+}
