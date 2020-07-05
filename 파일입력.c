23c4c6a1c13427a6652ea696aee78155f2c5f537
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-07-05 23:25:12 +0900

    파일입출력 연습

---
+++ 파일입력.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	FILE *fp = NULL;
+	fp = fopen("testfile.txt", "r");
+	if(fp == NULL)
+	{
+		printf("no file\n");
+		return 0;
+	}
+	printf("file opened\n");
+	char s[20];
+	while(1)
+	{
+		if(
+		fgets(s, 20, fp) == NULL)
+		{
+			break;
+		}
+		printf("%s", s);
+	}
+	fclose(fp);
+	
+	return 0;
+}
