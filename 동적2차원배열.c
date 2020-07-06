3e8677b04d46910d0b9ff848597c0dc73f8b1753
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-07-06 23:24:02 +0900

    동적메모리 연습

---
+++ 동적2차원배열.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i, x, y;
+	int **pptr = (int **)malloc(7 * sizeof(int*));
+	for(i = 0;i < 7;i++)
+	{
+		*(pptr + i) = (int *)malloc(5 * sizeof(int));
+	}
+	
+	for(y = 0;y < 7;y++)
+	{
+		for(x = 0;x < 5;x++)
+		{
+			*(*(pptr + y) + x) = 5 * y + x;
+		}
+	}
+	
+	for(y = 0;y < 7;y++)
+	{
+		for(x = 0;x < 5;x++)
+		{
+			printf("%3d", *(*(pptr + y) + x));
+		}
+		printf("\n");
+	}
+	
+	for(y = 0;y < 7;y++)
+	{
+		free(*(pptr + y));
+	}
+	free(pptr);
+	return 0;
+}
---
+++ 동적구조체.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+struct book
+{
+	int number;
+	char name[30];
+};
+
+void showbook(struct book *ps, int howmany)
+{
+	int i;
+	for(i = 0;i < howmany;i++)
+	{
+		printf("%d번 책 : %s", (ps + i)->number, (ps + i)->name);
+	}
+}
+
+int main()
+{
+	int howmany, i;
+	printf("입력할 책이 몇 권입니까? : ");
+	scanf("%d", &howmany);
+	getchar();
+	
+	struct book *ps;
+	ps = (struct book *)malloc(howmany * sizeof(struct book));
+	if(ps == NULL)
+	{
+		printf("동적메모리 할당에 실패했습니다.\n");
+		exit(1);
+	}
+	
+	for(i = 0;i < howmany;i++)
+	{
+		(ps + i) -> number = i + 1;
+		printf("%d번 책의 이름 입력 : ", i + 1);
+		fgets((ps + i) -> name, 30, stdin);
+	}
+	showbook(ps, howmany);
+	free(ps);
+	return 0;
+}
