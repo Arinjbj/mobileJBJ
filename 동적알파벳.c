b918c7b6ec9e84c281c2d43d9168177c40368175
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-07-06 21:40:43 +0900

    동적메모리 연습

---
+++ 동적알파벳.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char *pc;
+	int i;
+	pc = (char *)malloc(sizeof(char) * 100);
+	if(pc == NULL)
+	{
+		printf("동적메모리 할당 실패\n");
+		exit(1);
+	}
+	for(i = 0;i < 26;i++)
+	{
+		*(pc + i) = 'a' + i;
+	}
+	*(pc + i) = NULL;
+	printf("%s\n", pc);
+	free(pc);
+	return 0;
+}
---
+++ 동적정수5개.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int *pi;
+	int i;
+	pi = (int *)malloc(sizeof(int) * 5);
+	printf("정수를 입력하세요\n");
+	for(i = 0;i < 5;i++)
+	{
+		printf("입력 : ");
+		scanf("%d", pi + i);
+	}
+	for(i = 0;i < 5;i++)
+	{
+		printf("%d ", *(pi + i));
+	}
+	free(pi);
+	for(i = 0;i < 5;i++)
+	{
+		printf("%d ", *(pi + i));
+	}
+	return 0;
+}
